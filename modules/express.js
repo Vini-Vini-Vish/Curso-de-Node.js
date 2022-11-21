bd8161b86def00c94f53cd82226ef8ab3d5e1246
Vinícius Henrique do Nascimento <vinicius_henrique889@hotmail.com>
2022-11-21 17:53:21 -0300

    Find, Update e Delete
    Parte de Update e Delete de dados, buscar por todos os registros e busca por id

--- modules/express.js
+++ modules/express.js
@@ -5,24 +5,26 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/home", (req, res) => {
-  res.contentType("application/html");
-  res.status(200).send("<h1>Hello World<h1>");
+app.get("/users", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+
+    res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).send(error.mensage);
+  }
 });
 
-app.get("/users", (req, res) => {
-  const users = [
-    {
-      name: "Pedro",
-      email: "pedro@gmail.com",
-    },
-    {
-      name: "Julio",
-      email: "Julio@gmail.com",
-    },
-  ];
+app.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
 
-  res.status(200).json(users);
+    const user = await UserModel.findById(id);
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error.mensage);
+  }
 });
 
 app.post("/users", async (req, res) => {
@@ -30,6 +32,30 @@ app.post("/users", async (req, res) => {
     const user = await UserModel.create(req.body);
 
     res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error.mensage);
+  }
+});
+
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.mensage);
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findByIdAndRemove(id);
+
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).send(error.mensage);
   }
